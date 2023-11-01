ad8668504f34c0261ff7305f32712691ffae2ae7
Basliel-Amsalu <acychappiekiller@gmail.com>
2023-11-01 21:04:16 +0300

    modified

--- Routes/api/auth.js
+++ Routes/api/auth.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
+const User = require("../../models/userModel");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("atuh route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    res.status(200).json({
+      message: "success",
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "server error",
+      err,
+    });
+  }
 });
 
 module.exports = router;
--- Routes/api/user.js
+++ Routes/api/user.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/userModel");
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const router = express.Router();
 
@@ -15,11 +20,55 @@ router.post(
   ],
 
   async (req, res) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
     try {
-        
-    } catch (err) {}
-    //   res.send("user route");
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          errors: [{ msg: "user already exists" }],
+        });
+      }
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      const newUser = await User.create({ avatar, name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+
+      newUser.password = await bcrypt.hash(password, salt);
+
+      await newUser.save();
+
+      const payload = {
+        user: {
+          id: newUser.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(201).json({
+            message: "Success",
+            user: newUser, // Include the user object
+            token,
+          });
+        }
+      );
+    } catch (err) {
+      res.status(500).send("server error");
+    }
   }
 );
 
